b3a8ad65b055af898210799d2eea884ae985463b
fikertt09 <135347597+fikertt09@users.noreply.github.com>
2023-07-25 21:16:03 +0300

    Find Duplicate File in System.cpp

--- Find Duplicate File in System.cpp
+++ Find Duplicate File in System.cpp
@@ -1,23 +1,20 @@
 class Solution {
- public:
-  vector<vector<string>> findDuplicate(vector<string>& paths) {
-    vector<vector<string>> ans;
+public:
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        vector<vector<string>> ans;
     unordered_map<string, vector<string>> contentToFilePaths;
 
     for (const string& path : paths) {
       istringstream iss(path);
       string rootPath;
-      iss >> rootPath;  // "root/d1/d2/.../dm"
+      iss >> rootPath;  
 
       string fileAndContent;
-      while (iss >> fileAndContent) {  // "fn.txt(fn_content)"
+      while (iss >> fileAndContent) { 
         const int l = fileAndContent.find('(');
         const int r = fileAndContent.find(')');
-        // "fn.txt"
         const string file = fileAndContent.substr(0, l);
-        // "fn_content"
         const string content = fileAndContent.substr(l + 1, r - l - 1);
-        // "root/d1/d2/.../dm/fn.txt"
         const string filePath = rootPath + '/' + file;
         contentToFilePaths[content].push_back(filePath);
       }
@@ -28,5 +25,5 @@ class Solution {
         ans.push_back(filePaths);
 
     return ans;
-  }
+    }
 };
